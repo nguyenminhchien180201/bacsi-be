3fa9c4218bb37e489faf4651e19e635d7e164edd
nguyenminhchien180201 <sayta.kinhdoanh@gmail.com>
2023-02-01 00:00:57 +0700

    eight

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,6 +1,6 @@
 import db from "../models/index";
 require('dotenv').config();
-import _ from 'lodash';
+import _, { reject } from 'lodash';
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 let getTopDoctorHome = (limitInput) => {
@@ -176,10 +176,38 @@ let bulkCreateSchedule = (data) => {
         }
     })
 }
+let getScheduleByDate=(doctorId,date)=>{
+    return new Promise(async(resolve,reject)=>{
+    try {
+        if(!doctorId||!date){
+            resolve({
+                errCode:1,
+                errMessage:'Missing required parameters'
+            })
+        }
+        else{
+            let dataSchedule=await db.Schedule.findAll({
+                where:{
+                    doctorId:doctorId,
+                    date:date
+                }
+            })
+            if (!dataSchedule)dataSchedule= [];
+            resolve({
+                errCode:0,
+                data:dataSchedule
+            })
+        }
+    } catch (error) {
+        reject(e)
+    }
+})
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
-    bulkCreateSchedule: bulkCreateSchedule
+    bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleByDate:getScheduleByDate
 }
