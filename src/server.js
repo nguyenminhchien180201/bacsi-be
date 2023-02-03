300ae44ccdbccbd2e85de3cb0b8aff9c25cb55a9
nguyenminhchien180201 <sayta.kinhdoanh@gmail.com>
2023-02-04 02:06:26 +0700

    ten

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ let app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Origin', 'https://doctor-oy5uvulzr-nguyenminhchien180201-gmailcom.vercel.app');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
